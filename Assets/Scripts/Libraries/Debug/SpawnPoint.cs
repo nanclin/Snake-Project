3f2b58eb2be5910663c052cd4c8c6209372ce52e
Nejc Anclin <nejc.anclin@gmail.com>
2015-05-20 12:37:33 +0200

    Debug DrawArrow class

---
+++ Assets/Scripts/Libraries/Debug/SpawnPoint.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnPoint : MonoBehaviour
+{
+	void OnDrawGizmos()
+	{
+		DrawArrow.ForGizmo( transform.position, transform.forward * 4, Color.blue, 1 );
+	}
+}
