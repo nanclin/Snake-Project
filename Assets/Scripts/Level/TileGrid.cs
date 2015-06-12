20f0ab29e5b320ff39e6a28c074a3566dc56f203
Nejc Anclin <nejc.anclin@gmail.com>
2015-06-12 21:27:53 +0200

    Generate map tile models referencing bug fix

--- Assets/Scripts/Level/TileGrid.cs
+++ Assets/Scripts/Level/TileGrid.cs
@@ -49,6 +49,22 @@ public class TileGrid : MonoBehaviour
 		new int[]{129,129,129,129,129,129,129,129,129,129,129,135,134,129,129,129,129,129,129,129,129,129,129,129,129}
 	};
 
+	private int[][] testMap = 
+	{
+		new int[]{1,2,3,4,5,6,7,8,9,10,11,12},
+		new int[]{13,14,15,16,17,18,19,20,21,22,23,24},
+		new int[]{25,26,27,28,29,30,31,32,33,34,35,36},
+		new int[]{37,38,39,40,41,42,43,44,45,46,47,48},
+		new int[]{49,50,51,52,53,54,55,56,57,58,59,60},
+		new int[]{61,62,63,64,65,66,67,68,69,70,71,72},
+		new int[]{73,74,75,76,77,78,79,80,81,82,83,84},
+		new int[]{85,86,87,88,89,90,91,92,93,94,95,96},
+		new int[]{97,98,99,100,101,102,103,104,105,106,107,108},
+		new int[]{109,110,111,112,113,114,115,116,117,118,119,120},
+		new int[]{121,122,123,124,125,126,127,128,129,130,131,132},
+		new int[]{133,134,135,136,137,138,139,140,141,142,143,144}
+	};
+
 	void Awake()
 	{
 		// Get prefab refenrence to instantiate from
@@ -66,7 +82,8 @@ public class TileGrid : MonoBehaviour
 		tile_knee_medium = transform.Find("tile_knee_medium");
 		tile_knee_large = transform.Find("tile_knee_large");
 
-		GenerateMap( map, 0 );
+		GenerateMap( testMap, 0 );
+		// GenerateMap( map, 0 );
 		// GenerateMap( map2, 1 );
 	}
 
@@ -120,7 +137,7 @@ public class TileGrid : MonoBehaviour
 					case 125:
 						tile = Instantiate( tile_peak_1, pos, Quaternion.identity ) as Transform;
 						break;
-					case 88:
+					case 126:
 						pos += new Vector3(1,0,0) * tileSize;
 						tile = Instantiate( tile_peak_1, pos, Quaternion.identity ) as Transform;
 						tile.eulerAngles = Vector3.up * 90;
@@ -284,7 +301,7 @@ public class TileGrid : MonoBehaviour
 						tile.eulerAngles = Vector3.up * 180;
 						x += 2;
 						break;
-					case 41:
+					case 43:
 						pos += new Vector3(0,0,-3) * tileSize;
 						tile = Instantiate( tile_corner_large, pos, Quaternion.identity ) as Transform;
 						tile.eulerAngles = Vector3.up * 270;
