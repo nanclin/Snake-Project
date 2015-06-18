a19f4069ca4ecf7a059b1de739b125f5c13e1116
Nejc Anclin <nejc.anclin@gmail.com>
2015-06-18 11:41:06 +0200

    Load levels system

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -14,10 +14,10 @@ public class GameManager : MonoBehaviour {
 	public static float START_TIME;
 
 	// Static
-	public static bool FSM_DEBUG = false;
+	public static bool FSM_DEBUG = true;
 	
 	// System
-	public enum GameState { Idle, NewGame, Run, GameOver, OpenExit, LevelFinished }
+	public enum GameState { Idle, Loading, SplashScreen, MapScreen, NewGame, Run, GameOver, OpenExit, LevelFinished }
 	private GameState _state;
 	private float sw = Screen.width;
 	private float sh = Screen.height;
@@ -33,19 +33,29 @@ public class GameManager : MonoBehaviour {
 
 	void Awake()
 	{
-		INSTANCE = this;
+		DontDestroyOnLoad( transform.gameObject );
+
+		if( INSTANCE == null ){
+			INSTANCE = this;
+		}
 	}
 
 	// Use this for initialization
 	void Start ()
 	{
-		LoadLevel();
+		// LoadLevel( "Level 01" );
 
-		currentState = GameState.NewGame;
+		currentState = GameState.SplashScreen;
+		// currentState = GameState.NewGame;
 		// currentState = GameState.Run;
 	}
 
-	// Update is called once per frame
+	void Update()
+	{
+		if( Input.GetKeyDown("o") )
+			currentState = GameState.OpenExit;
+	}
+
 	void FixedUpdate ()
 	{
 		ExecuteState();
@@ -53,20 +63,59 @@ public class GameManager : MonoBehaviour {
 
 	void OnGUI ()
 	{
-		if( currentState == GameState.NewGame ){
+		GUI.Box( new Rect( 0, 0, 100, Screen.width/3 ), currentState.ToString() );
+
+		// SPLASH SCREEN GUI ///////////////////////////////////////////////////////////////
+		if( currentState == GameState.SplashScreen )
+		{
+			GUI.Box( new Rect( 0, 0, sw, sh ), "Snake Project\n2015\nNejc Anclin" );
+
+			if( GUI.Button( new Rect( sw/2-bw/2, sh/2-bh/2, bw, bh ), "Start" ) ){
+				LoadLevel( "Map Screen" );
+			}
+		}
+		//////////////////////////////////////////////////////////// EO SPLASH SCREEN GUI //
+
+		// MAP SCREEN GUI ///////////////////////////////////////////////////////////////
+		if( currentState == GameState.MapScreen )
+		{
+			GUI.Box( new Rect( 0, 0, sw, sh ), "Map Screen" );
+
+			if( GUI.Button( new Rect( sw/2-bw/2, sh/2-bh/2, bw, bh / 2 ), "Level 01" ) ){
+				LoadLevel( "Level 01" );
+			}
+			if( GUI.Button( new Rect( sw/2-bw/2, sh/2-bh/2 + 100, bw, bh / 2 ), "Level 02" ) ){
+				LoadLevel( "Level 02" );
+			}
+		}
+		//////////////////////////////////////////////////////////// EO MAP SCREEN GUI //
+
+		// NEW GAME GUI ///////////////////////////////////////////////////////////////
+		if( currentState == GameState.NewGame )
+		{
 			GUI.Box( new Rect( 0, 0, sw, sh ), "Snake Project\nMay 2015\nNejc Anclin" );
+
 			if( GUI.Button( new Rect( sw/2-bw/2, sh/2-bh/2, bw, bh ), "Begin Game" ) ){
 				currentState = GameState.Run;
 			}
 		}
+		//////////////////////////////////////////////////////////// EO NEW GAME GUI //
+		
+		// GAME OVER GUI ///////////////////////////////////////////////////////////////
 		else if( currentState == GameState.GameOver )
 		{	
 			GUI.Box( new Rect( 0, 0, sw, sh ), "\n\n\n\nGame Over" );
-			if( GUI.Button( new Rect( sw/2-bw/2, sh/2-bh/2, bw, bh ), "New Game " ) ){
+
+			if( GUI.Button( new Rect( sw/2-bw/2, sh/2-bh/2, bw, bh / 2 ), "Restart" ) ){
 				currentState = GameState.NewGame;
 			}
+			if( GUI.Button( new Rect( sw/2-bw/2, sh/2-bh/2 + 100, bw, bh / 2 ), "Quit" ) ){
+				LoadLevel( "Map Screen" );
+			}
 		}
+		//////////////////////////////////////////////////////////// EO GAME OVER GUI //
 
+		// GAME RUNNING GUI ///////////////////////////////////////////////////////////////
 		if( currentState == GameState.Run ||
 			currentState == GameState.GameOver ){
 			GUI.Box( new Rect( sw/2 - 100/2 - 60, 10, 100, 25 ), "Score: " + SCORE );
@@ -76,7 +125,9 @@ public class GameManager : MonoBehaviour {
 			int s = (int)( TIME % 60f );
 			GUI.Box( new Rect( sw/2 - 100/2 + 180, 10, 100, 25 ), "Time: " + m + ":" + s );
 		}
+		//////////////////////////////////////////////////////////// EO GAME RUNNING GUI //
 
+		// LEVEL FINISHED GUI ///////////////////////////////////////////////////////////////
 		if( currentState == GameState.LevelFinished ){
 			GUI.Box( new Rect( 0, 0, sw, sh ), "\n\n\n\nCONGRATULATIONS!\n You finished this level!\nYour score is\n" + SCORE );
 
@@ -84,10 +135,15 @@ public class GameManager : MonoBehaviour {
 			int s = (int)( TIME % 60f );
 			GUI.Box( new Rect( sw/2 - 100/2 + 180, 10, 100, 25 ), "Time: " + m + ":" + s );
 			
-			if( GUI.Button( new Rect( sw/2-bw/2, sh/2-bh/2, bw, bh ), "New Game " ) ){
+			if( GUI.Button( new Rect( sw/2-bw/2, sh/2-bh/2, bw, bh / 2 ), "Continue" ) ){
+				LoadLevel( "Map Screen" );
+			}
+
+			if( GUI.Button( new Rect( sw/2-bw/2, sh/2-bh/2 + 100, bw, bh / 2 ), "Restart" ) ){
 				currentState = GameState.NewGame;
 			}
 		}
+		//////////////////////////////////////////////////////////// EO LEVEL FINISHED GUI //
 	}
 //////////////////////////////////////////////////////////// EO UNITY METHODS //
 
@@ -120,6 +176,15 @@ public class GameManager : MonoBehaviour {
 		// print("EnterState: " + state);
 		switch( state )
 		{
+			case GameState.Loading:
+				LoadingEnterState();
+				break;
+			case GameState.MapScreen:
+				MapScreenEnterState();
+				break;
+			case GameState.SplashScreen:
+				SplashScreenEnterState();
+				break;
 			case GameState.NewGame:
 				NewGameEnterState();
 				break;
@@ -142,6 +207,15 @@ public class GameManager : MonoBehaviour {
 	{
 		switch( currentState )
 		{
+			case GameState.Loading:
+				LoadingState();
+				break;
+			case GameState.SplashScreen:
+				SplashScreenState();
+				break;
+			case GameState.MapScreen:
+				MapScreenState();
+				break;
 			case GameState.NewGame:
 				NewGameState();
 				break;
@@ -164,6 +238,15 @@ public class GameManager : MonoBehaviour {
 	{
 		switch( state )
 		{
+			case GameState.Loading:
+				LoadingExitState();
+				break;
+			case GameState.SplashScreen:
+				SplashScreenExitState();
+				break;
+			case GameState.MapScreen:
+				MapScreenExitState();
+				break;
 			case GameState.NewGame:
 				NewGameExitState();
 				break;
@@ -189,27 +272,8 @@ public class GameManager : MonoBehaviour {
 
 // OTHER METHODS ///////////////////////////////////////////////////////////////
 
-	private void LoadLevel()
+	private void ResetLevel()
 	{
-		// Load scene
-		// [code...]
-
-		// SET LEVEL REFERENCES ///////////////////////////////////////////////////////////////
-		snake = GameObject.Find("Player Snake").GetComponent<SnakeController>();
-		exit = GameObject.FindWithTag("Exit");
-		if( exit != null )
-			exit.SetActive( false );
-		//////////////////////////////////////////////////////////// EO SET LEVEL REFERENCES //
-	}
-//////////////////////////////////////////////////////////// EO OTHER METHODS //
-
-// STATE METHODS ////////////////////////////////////////////////////////
-
-// NEW GAME STATE //
-	private void NewGameEnterState()
-	{
-		DebugEnter( "NewGame" );
-
 		// Reset score
 		GameManager.SCORE = 0;
 		GameManager.STARS = 0;
@@ -233,11 +297,107 @@ public class GameManager : MonoBehaviour {
 		// cameraHolder.transform.position = snakeSpawnPoint.position;
 
 		// Reset exit
-		if( exit )
+		if( exit != null )
 			exit.SetActive( false );
 
 		// Automatically start running game
-		// currentState = GameState.Run;
+		// currentState = GameState.Run;		
+	}
+
+	private void LoadLevel( string level )
+	{
+		currentLevel = level;
+		currentState = GameState.Loading;
+		// // Load scene
+		// Application.LoadLevel( Application.loadedLevel + 1 );		
+	}
+//////////////////////////////////////////////////////////// EO OTHER METHODS //
+
+// STATE METHODS ////////////////////////////////////////////////////////
+
+
+// LOADING STATE //
+
+	private AsyncOperation async;
+	private string currentLevel;
+
+	private void LoadingEnterState()
+	{
+		DebugEnter( "Loading" );
+
+		async = Application.LoadLevelAsync( currentLevel );
+	}
+
+	private void LoadingState()
+	{
+		DebugExecute( "Loading" );
+
+		if( async.isDone ){
+			switch( currentLevel )
+			{
+				case "Splash Screen":
+					break;
+				case "Map Screen":
+					currentState = GameState.MapScreen;
+					break;
+				default:
+
+					// SET LEVEL REFERENCES ///////////////////////////////////////////////////////////////
+					snake = GameObject.Find("Player Snake").GetComponent<SnakeController>();
+					exit = GameObject.FindWithTag("Exit");
+					//////////////////////////////////////////////////////////// EO SET LEVEL REFERENCES //
+
+					currentState = GameState.NewGame;
+					break;
+			}
+		}
+	}
+
+	private void LoadingExitState()
+	{
+		DebugExit( "Loading" );
+	}
+// EO LOADING STATE //
+
+// SPLASH SCREEN STATE //
+	private void SplashScreenEnterState()
+	{
+		DebugEnter( "SplashScreen" );
+	}
+
+	private void SplashScreenState()
+	{
+		DebugExecute( "SplashScreen" );
+	}
+
+	private void SplashScreenExitState()
+	{
+		DebugExit( "SplashScreen" );
+	}
+// EO SPLASH SCREEN STATE //
+
+// MAP SCREEN STATE //
+	private void MapScreenEnterState()
+	{
+		DebugEnter( "MapScreen" );
+	}
+
+	private void MapScreenState()
+	{
+		DebugExecute( "MapScreen" );
+	}
+
+	private void MapScreenExitState()
+	{
+		DebugExit( "MapScreen" );
+	}
+// EO MAP SCREEN STATE //
+
+// NEW GAME STATE //
+	private void NewGameEnterState()
+	{
+		DebugEnter( "NewGame" );
+		ResetLevel();
 	}
 
 	private void NewGameState()
@@ -304,7 +464,10 @@ public class GameManager : MonoBehaviour {
 		snake.currentState = SnakeState.Idle;
 
 		// Enable exit
-		exit.SetActive( true );
+		if( exit != null )
+			exit.SetActive( true );
+		else
+			Debug.LogError("Exit missing!");
 	}
 
 	private void OpenExitState()
@@ -313,7 +476,7 @@ public class GameManager : MonoBehaviour {
 
 
 		float timeElapsed = Time.time - openTime;
-		if( timeElapsed >= 1 )
+		if( timeElapsed >= 0.3f )
 			currentState = GameState.Run;
 		// else if( timeElapsed >= 4 )
 		// 	cameraHolder.GetComponent<FollowTarget>().target = snake.transform;
